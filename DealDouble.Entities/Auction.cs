1a1b3ad57c927c7b8f98fb9880124ac7fc1bcb55
Gurinder Singh <gs38500@gmail.com>
2021-08-12 18:45:57 +0530

    second commit

--- DealDouble.Entities/Auction.cs
+++ DealDouble.Entities/Auction.cs
@@ -8,5 +8,11 @@ namespace DealDouble.Entities
 {
     class Auction
     {
+        public string Title { get; set; }
+        public string PictureURL { get; set; }
+        public string Description { get; set; }
+        public decimal ActualAmount { get; set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }

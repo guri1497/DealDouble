b953b12294f154e360f356378a38ba9dfc704112
gurinder system 3 <dev.gurinder@gmail.com>
2021-08-15 17:57:36 +0530

    crud complete

---
+++ DealDouble.Services/AuctionServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealDouble.Data;
+using DealDouble.Entities;
+
+namespace DealDouble.Services
+{
+    public class AuctionServices
+    {
+        public Auction GetAuctionByID(int id)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            return context.Auctions.Find(id);
+           
+        }
+
+        public List<Auction> GetAllAuctions()
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            return context.Auctions.ToList();
+
+        }
+        public void SaveAuction(Auction auction)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            context.Auctions.Add(auction);
+            context.SaveChanges();
+        }
+
+        public void UpdateAuction(Auction auction)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            context.Entry(auction).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteAuction(Auction auction)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            context.Entry(auction).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
+}
--- DealDouble.Web/Controllers/AuctionsController.cs
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -1,4 +1,5 @@
 ﻿using DealDouble.Entities;
+using DealDouble.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,16 @@ namespace DealDouble.Web.Controllers
 {
     public class AuctionsController : Controller
     {
+        AuctionServices auctionServices = new AuctionServices();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = auctionServices.GetAllAuctions();
+            return View(model);
+        }
+
+
         [HttpGet]
         public ActionResult Create()
         {
@@ -18,7 +29,37 @@ namespace DealDouble.Web.Controllers
         [HttpPost]
         public ActionResult Create(Auction auction)
         {
+            auctionServices.SaveAuction(auction);
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var model = auctionServices.GetAuctionByID(ID);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            AuctionServices auctionServices = new AuctionServices();
+            auctionServices.SaveAuction(auction);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var model = auctionServices.GetAuctionByID(ID);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            auctionServices.DeleteAuction(auction);
+            return RedirectToAction("Index");
         }
     }
 }
